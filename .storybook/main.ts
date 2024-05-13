83a7888b0f7a4f55163fda62aaad09a6716434b2
Abdul Haseeb <mianhaseeeb302@gmail.com>
2024-05-13 18:05:16 +0500

    added input and layout to library

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -20,10 +20,12 @@ const config: StorybookConfig = {
   },
   viteFinal: async (config) => {
     config.plugins?.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
       tsconfigPaths({
-        projects: [path.resolve(path.dirname(__dirname), "tscongig.json")],
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
       })
     );
+
     return config;
   },
 };
